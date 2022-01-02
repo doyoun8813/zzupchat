d8c5becb967ed5b6f9d0c514098094d2050a17c3
doyoun8813 <doyoun8813@gmail.com>
2022-01-02 14:50:45 +0900

    app.js create

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-hi
+const express = require('express');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '빈 포트에서 대기 중');
+});
